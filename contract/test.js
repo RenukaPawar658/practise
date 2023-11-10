4bd6b3da9185f44be36f8aba3511efa7c64fee4a
Renuka Pawar <112619124+RenukaPawar658@users.noreply.github.com>
2023-11-10 09:28:42 +0530

    checking adtional code

--- contract/test.js
+++ contract/test.js
@@ -1,5 +1,31 @@
 const { expect } = require("chai");
 const { ethers } = require("ethers");
+/*   
+const { ethers } = require("hardhat"); 
+
+describe("ERC1155", function () {
+
+  let erc1155;
+  let owner;
+
+  beforeEach(async function () {
+
+    // Get contract factory from source
+    const ERC1155Contract = await ethers.getContractFactory("MyERC1155");
+
+    // Deploy a new instance
+    erc1155 = await ERC1155Contract.deploy();
+
+    [owner] = await ethers.getSigners();
+
+  });
+
+  // Tests...
+
+});
+
+
+*/
 
 // Import compiled contract artifacts 
 const MyERC1155 = artifacts.require("MyERC1155");
