e0e30521ee4d0a4a4b1f2da692455c5c9ef9ff80
Renuka Pawar <112619124+RenukaPawar658@users.noreply.github.com>
2023-11-10 09:24:43 +0530

    Create test.js

---
+++ contract/test.js
@@ -0,0 +1,46 @@
+const { expect } = require("chai");
+const { ethers } = require("ethers");
+
+// Import compiled contract artifacts 
+const MyERC1155 = artifacts.require("MyERC1155");
+
+describe("MyERC1155", function () {
+
+  let erc1155;
+  let owner, account1, account2;
+
+  beforeEach(async function () {
+    // Get contract and buyer/seller accounts
+    [owner, account1, account2] = await ethers.getSigners();
+
+    // Deploy contract
+    erc1155 = await MyERC1155.deploy();    
+  });
+
+  describe("mint", function () {
+
+    it("should mint token", async function () {
+      await erc1155.mint(account1.address, 1, 10);
+      
+      // Check balance
+      expect(await erc1155.balanceOf(account1.address, 1)).to.equal(10);
+    });
+
+  });
+  
+  describe("burn", function () {
+
+    beforeEach(async function () {
+      await erc1155.mint(account1.address, 1, 10); 
+    });
+
+    it("should burn token", async function () {
+      await erc1155.burn(account1.address, 1, 5);
+
+      // Check balance  
+      expect(await erc1155.balanceOf(account1.address, 1)).to.equal(5); 
+    });
+
+  });
+  
+});
